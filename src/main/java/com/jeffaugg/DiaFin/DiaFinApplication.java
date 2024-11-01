19497d53a44f3c62d90e1c9bd44ea95ae847edfc
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-11-01 11:24:28 -0300

    Initial Commit

---
+++ src/main/java/com/jeffaugg/DiaFin/DiaFinApplication.java
@@ -0,0 +1,13 @@
+package com.jeffaugg.DiaFin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaFinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaFinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeffaugg/DiaFin/DiaFinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffaugg.DiaFin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaFinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
